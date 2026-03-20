068d64a584a334cbd8cf1a298a85d1ae89ff50ab
Matthieu Basset <matthieu.basset@proton.me>
2026-03-20 12:32:56 +0100

    Logging

--- src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
@@ -1,12 +1,23 @@
 package guru.springframework.spring7restmvc.controller;
 
+import guru.springframework.spring7restmvc.model.Beer;
 import guru.springframework.spring7restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @Controller
 @AllArgsConstructor
 public class BeerController {
 
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+        log.debug("Get Beer by ID - Controller");
+
+        return beerService.getBeerById(id);
+    }
 }
