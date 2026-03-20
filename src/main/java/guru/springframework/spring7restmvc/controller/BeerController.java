d584e7d44bd04c88393c63102f250f64b1cde208
Matthieu Basset <matthieu.basset@proton.me>
2026-03-20 14:28:16 +0100

    Assignment: Custommer GET method

--- src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring7restmvc/controller/BeerController.java
@@ -4,27 +4,30 @@ import guru.springframework.spring7restmvc.model.Beer;
 import guru.springframework.spring7restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@RestController
 @AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
-
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beers")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
-    
-    public Beer getBeerById(UUID id) {
+
+    @RequestMapping(value = "{beerID}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerID") UUID beerID) {
         log.debug("Get Beer by ID - Controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerID);
     }
 }
